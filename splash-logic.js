d8126d4be131ad19a7d2a1566f436ef49b4a0012
Kyle Richey <richey.kyle.i@gmail.com>
2017-07-10 21:30:29 -0700

    MVPMVPMVPMVPMVPMVPMVMVPMVPMVPMVMV

--- splash-logic.js
+++ splash-logic.js
@@ -5,16 +5,16 @@ console.log(loginButtonEl);
 
 loginButtonEl.addEventListener('submit', handleButton);
 
-var userNameValueExample = 'username';
-var stringifiedUsername = JSON.stringify(userNameValueExample);
-localStorage.username = stringifiedUsername;
+// var userNameValueExample = 'username';
+// var stringifiedUsername = JSON.stringify(userNameValueExample);
+// localStorage.username = stringifiedUsername;
 
 console.log(localStorage.username);
 
 function handleButton(event) {
   event.preventDefault();
-  var usersUsername = JSON.parse(localStorage.user.username);
-  if(event.target.username.value == usersUsername) {
+  var usersUsername = JSON.parse(localStorage.user);
+  if(event.target.username.value == usersUsername.username) {
     window.location.assign('story.html');
   } else {
     alert('Invalid Username. If you haven\'t already created a Username, click \'Create Character\' and start creating your story!');
